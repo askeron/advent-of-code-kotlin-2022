76e294ebb6c95764e2870f6adb33374b395e0bac
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-12-04 16:35:50 +0100

    Day04 improvements

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,17 +3,15 @@ class Day04 : Day<Int>(2, 4) {
     private fun parseInput(input: List<String>) = input.map { line -> line
         .split(",")
         .map { it.split('-').map { it.toInt() }.exactPair() }
-        .map { (a, b) ->  (a..b).toList() }
+        .map { (a, b) ->  (a..b).toSet() }
         .exactPair()
     }
 
     override fun part1(input: List<String>): Int {
-        return parseInput(input)
-            .count { (a, b) -> a.all { it in b } || b.all { it in a } }
+        return parseInput(input).count { (a, b) -> a.containsAll(b) || b.containsAll(a) }
     }
 
     override fun part2(input: List<String>): Int {
-        return parseInput(input)
-            .count { (a, b) -> a.any { it in b } || b.any { it in a } }
+        return parseInput(input).count { (a, b) -> a.intersect(b).isNotEmpty() }
     }
 }

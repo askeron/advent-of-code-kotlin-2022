7213c287487904aa7c841b240b3ef1e36bee727f
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-12-04 14:11:42 +0100

    added skeleton

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,15 @@
 fun main() {
-    // TODO: improve via reflection
-    listOf(
-        Day01(),
-        Day02(),
-        Day03(),
-    ).forEach { it.run() }
+    fun getDayInstance(dayNumber: Int): Day<*>? {
+        return try {
+            Class.forName("Day${dayNumber.toString().padStart(2, '0')}")
+                .getDeclaredConstructor().newInstance() as Day<*>
+        } catch (e: ClassNotFoundException) {
+            null
+        }
+    }
+
+    (25 downTo 1).asSequence()
+        .mapNotNull { getDayInstance(it) }
+        .first()
+        .run()
 }

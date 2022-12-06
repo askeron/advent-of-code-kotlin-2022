297e0b7858e0af5fc0b4c49f9ae33ab98b98e8e4
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-12-06 12:30:26 +0100

    Day06

---
+++ src/Day06.kt
@@ -0,0 +1,18 @@
+class Day06 : Day<Int>(7, 19) {
+    private fun parseInput(input: List<String>): String {
+        return input[0]
+    }
+
+    private fun String.indexOfFirstDistinctChars(count: Int) = toCharArray()
+        .toList()
+        .windowed(count)
+        .indexOfFirst { it.allDistinct() } + count
+
+    override fun part1(input: List<String>): Int {
+        return parseInput(input).indexOfFirstDistinctChars(4)
+    }
+
+    override fun part2(input: List<String>): Int {
+        return parseInput(input).indexOfFirstDistinctChars(14)
+    }
+}

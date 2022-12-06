de462bcf010704f91e7d343b0f11e0918a054ea0
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-12-06 15:08:43 +0100

    Day05 improvements

--- src/Utils.kt
+++ src/Utils.kt
@@ -82,11 +82,8 @@ fun List<Int>.toIntByDigits(): Int {
 
 fun <T> List<T>.nonUnique() = this.groupingBy { it }.eachCount().filter { it.value > 1 }
 
-fun <T> MutableList<T>.removeLast(n: Int): List<T> {
-    val result = mutableListOf<T>()
-    repeat(n) { result += removeLast() }
-    return result.toList().reversed()
-}
+fun <T> MutableList<T>.removeLast(n: Int): List<T> = mutableListOf<T>()
+    .also { repeat(n) { _ -> it += removeLast() } }.toList().reversed()
 
 fun <T> List<List<T>>.turnMatrix(): List<List<T>> = (0 until this[0].size).map { i -> this.map { it[i] } }
 

c56833709f38ca0f267db4a1e672d1dec098defc
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-12-13 23:47:57 +0100

    Day10 refactoring: renaming

--- src/Day10.kt
+++ src/Day10.kt
@@ -12,14 +12,14 @@ class Day10 : Day<String>("13140", """
 #....#.#..#..#.#....#..#.#..#.#....#....
 ####.#..#..##..#....###...##..#....####.""".removePrefix("\n")
 ) {
-    private fun getXValueForCycle(input: List<String>): List<Int> = input
+    private fun getXValueForCycles(input: List<String>): List<Int> = input
         .flatMap { if (it == "noop") listOf(0) else listOf(0, it.split(" ")[1].toInt()) }
         .scan(1, Int::plus)
 
     override fun part1(input: List<String>): String {
-        val xValueForCycle = getXValueForCycle(input)
-        return (20..xValueForCycle.size step 40).asSequence()
-            .map { it to xValueForCycle[it - 1] }
+        val xValueForCycles = getXValueForCycles(input)
+        return (20..xValueForCycles.size step 40).asSequence()
+            .map { it to xValueForCycles[it - 1] }
             //.onEach { println(it) }
             .map { (a, b) -> a * b }
             //.onEach { println(it) }
@@ -28,7 +28,7 @@ class Day10 : Day<String>("13140", """
     }
 
     override fun part2(input: List<String>): String {
-        return getXValueForCycle(input).mapIndexed { cycle, x -> (cycle % 40) in (x - 1)..(x + 1) }
+        return getXValueForCycles(input).mapIndexed { cycle, x -> (cycle % 40) in (x - 1)..(x + 1) }
             .dropLast(1)
             .joinToString("") { if (it) "#" else "." }
             .chunked(40)
